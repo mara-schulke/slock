5e74a202914877922e07978be1363fa99ebb8803
Maximilian Schulke <hello@maximilianschulke.com>
2021-06-16 14:24:24 +0200

    Update lockscreen colors

--- config.def.h
+++ config.def.h
@@ -3,9 +3,9 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#1a1b1d",     /* after initialization */
+	[INPUT] =  "#a03e3e",   /* during input */
+	[FAILED] = "#85678f",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
